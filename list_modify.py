c3f21b0e990e90db002fc15e92c6edc3e14136e4
santiagoyamamoto <santiagoyamamoto08@gmail.com>
2026-05-03 21:16:42 -0300

    Update list_modify.py

--- list_modify.py
+++ list_modify.py
@@ -12,7 +12,11 @@ def put(value, lst):
         put("Blue", colors) -> 1
         # colors ahora es ["Red", "Blue", "Green"]
     """
-    return "ANSWER HERE"  # Remove this line and implement
+    for i in range(len(lst)):
+        if lst[i] == "":
+            lst[i] = value
+            return i
+    return -1
 
 
 def remove(value, lst):
@@ -26,4 +30,11 @@ def remove(value, lst):
         remove("Red", colors) -> 2
         # colors ahora es ["", "Green", "", "Blue"]
     """
-    return "ANSWER HERE"  # Remove this line and implement
+    cant = 0
+    for i in range(len(lst)):
+        if lst[i] == value:
+            lst[i] = ""
+            cant = cant + 1
+    return cant
+
+

8628071575c86d5e2c6bd8f5e0e274bb5ae45470
santiagoyamamoto <santiagoyamamoto08@gmail.com>
2026-05-03 21:26:55 -0300

    Update nested_loops.py

--- nested_loops.py
+++ nested_loops.py
@@ -34,3 +34,25 @@ def row_sums(matrix):
 
         lista.append(total)
     return lista
+
+
+
+def col_sums(matrix):
+    """
+    Dada una matriz (lista de listas de numeros), retorna una lista
+    donde cada elemento es la suma de la columna correspondiente.
+    Se asume que todas las filas tienen la misma longitud.
+
+    Ejemplo: col_sums([[1, 2, 3], [4, 5, 6]]) -> [5, 7, 9]
+    """
+
+    resultados = []
+
+    for j in range(len(matrix[0])):
+        suma_actual = 0
+
+        for i in range(len(matrix)):
+            suma_actual += matrix[i][j]
+
+        resultados.append(suma_actual)
+    return resultados

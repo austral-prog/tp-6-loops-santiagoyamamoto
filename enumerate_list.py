067f02a1958b3588bc3109ce74713f1171d1ca7f
santiagoyamamoto <santiagoyamamoto08@gmail.com>
2026-05-03 21:13:56 -0300

    Update enumerate_list.py

--- enumerate_list.py
+++ enumerate_list.py
@@ -9,7 +9,13 @@ def enumerate_list(lst):
 
     Ejemplo: enumerate_list(["Red", "Green", "", "White"]) -> ["0. Red", "1. Green", "2. White"]
     """
-    return "ANSWER HERE"  # Remove this line and implement
+    lista = []
+    indice = -1
+    for elemento in lst:
+        if elemento != "":
+            indice = indice + 1
+            lista.append (f"{indice}. {elemento}")
+    return lista
 
 
 def enumerate_backwards(lst):
@@ -19,4 +25,16 @@ def enumerate_backwards(lst):
 
     Ejemplo: enumerate_backwards(["Red", "Green", ""]) -> ["0. deR", "1. neerG"]
     """
-    return "ANSWER HERE"  # Remove this line and implement
+
+    lista = []
+
+    indice = -1
+
+    for elemento in lst:
+        if elemento != "":
+            word = ""
+            indice = indice +1
+            for letras in elemento:
+                word = letras + word
+            lista.append(f"{indice}. {word}")
+    return lista

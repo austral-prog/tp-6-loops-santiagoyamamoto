f206801de6c2a042fe227cf31f7e6052c02c51c2
santiagoyamamoto <santiagoyamamoto08@gmail.com>
2026-05-03 21:29:40 -0300

    Update enumerate_list.py

--- enumerate_list.py
+++ enumerate_list.py
@@ -3,7 +3,7 @@
 def enumerate_list(lst):
     """
     Dada una lista de strings, retorna una nueva lista donde cada elemento
-    tiene el formato "indice. valor". Los strings vacios se deben saltear
+    tiene el formato "indice. valor". Los strings vacios se deben saltear 
     y no deben aparecer en la lista resultante.
     El indice debe ser consecutivo (no el indice original).
 
